7f321941c75678c0c50cdddbc801056b6bba367b
Dinesh Danda <dinesh.wr150@gmail.com>
2019-09-09 21:32:47 -0500

    Initial Commit

---
+++ HeartrateTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeartrateTracker
+//
+//  Created by dinesh danda on 9/9/19.
+//  Copyright © 2019 Dinesh Danda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
